5a2459501582ee0de18c6650019c328b65451597
Morariu Alexandru <93342935+AlexandruMorariuM@users.noreply.github.com>
2023-05-14 17:07:57 +0300

    Start
    Am inceput de la tema 2

---
+++ LaboratorAPI-main/DataLayer/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+    public class User:BaseEntity
+    {
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public Role Role { get; set; }
+    }
+}
